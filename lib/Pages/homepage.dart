af12e25dd7a5730b62eb4fd8f51edce23b59cf28
Tunjiy <rolatunji.tech@gmail.com>
2024-06-26 10:25:16 +0000

    homepage.dart

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -6,36 +6,9 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      drawer: Drawer(
-        backgroundColor: Colors.deepPurple[100],
-        child: Column(children: [
-          DrawerHeader(
-            child: Icon(
-              Icons.favorite,
-              size: 50,
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Firstpage'),
-            onTap: () {
-              //pop or close drawer first
-              Navigator.pop(context);
-
-              Navigator.pushNamed(context, '/firstpage');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
-            onTap: () {
-              Navigator.pushNamed(context, '/settingpage');
-            },
-          )
-        ]),
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Text('Homepage'),
       ),
     );
   }
