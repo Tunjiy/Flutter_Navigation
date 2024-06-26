9f0c274a2938587c36ead8aabf6b299028222d94
Tunjiy <rolatunji.tech@gmail.com>
2024-06-26 10:24:32 +0000

    profile.dart

---
+++ lib/Pages/Profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Profilepage extends StatelessWidget {
+  const Profilepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Text('Profilepage'),
+      ),
+    );
+  }
+}
